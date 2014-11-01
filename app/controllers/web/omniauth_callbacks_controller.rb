daba3d1f084983f0e856e11926c23cb81c4eb7b0
Manish Johari <manish.johari@kiwitech.com>
2014-11-01 09:28:25 +0530

    5- divide modules seperately for web,  api and Admin.
    Signed-off-by: Manish Johari <manish.johari@kiwitech.com>

--- app/controllers/web/omniauth_callbacks_controller.rb
+++ app/controllers/web/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Web::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     skip_before_filter :authenticate_user!
     def all
