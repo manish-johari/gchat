a0523e94772550be21327c9e94489ced72660736
Manish Johari <manish.johari@kiwitech.com>
2014-11-01 16:42:55 +0530

    6- functionality to confirm email via signup process via email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
+         :confirmable
 
   has_many :profile_photos
 
