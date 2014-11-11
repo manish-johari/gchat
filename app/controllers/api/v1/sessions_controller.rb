c61d6f86091d9ee1e4423dfb46e39119f6ba138d
Manish Johari <manish.johari@kiwitech.com>
2014-11-11 15:17:41 +0530

    Fb signin via facebook api

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::SessionsController < Devise::SessionsController
   skip_before_filter :verify_authenticity_token, :only => [:create, :destroy]
 
   def create
-    self.resource = warden.authenticate!(auth_options)
+    if params[:provider] == 'facebook'
+      user = User.from_omniauth(fb_data, current_user)
+      sign_in(user) if user.persisted?
+    else
+      self.resource = warden.authenticate!(auth_options)
+    end
   end
 
   def destroy
@@ -13,4 +18,30 @@ class Api::V1::SessionsController < Devise::SessionsController
           }
     end
   end
+
+  private
+
+  def fb_data
+    auth = {"provider"=>"#{params["provider"]}",
+             "uid"=>"#{params["fb_user_id"]}",
+             "info"=>
+              {"email"=>"#{params["email"]}",
+               "first_name"=>"#{params["first_name"]}",
+               "last_name"=>"#{params["last_name"]}",
+               "image"=>"#{params["image_url"]}",
+               },
+             "credentials"=>
+              {"token"=>
+                "#{params["fb_access_token"]}",
+              },
+             "extra"=>
+              {"raw_info"=>
+                {
+                 "gender"=>"#{params["gender"]}"
+                }
+              }
+            }
+    return auth
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,18 @@ class User < ActiveRecord::Base
   has_one :profile
 
   def self.from_omniauth(auth, current_user)
-    authorization = Authorization.where(:provider => auth.provider, :uid => auth.uid.to_s, :token => auth.credentials.token, :secret => auth.credentials.secret).first_or_initialize
+    authorization = Authorization.where(:provider => auth["provider"], :uid => auth["uid"].to_s, :token => auth["credentials"]["token"], :secret => auth["credentials"]["secret"]).first_or_initialize
     if authorization.user.blank?
       user = current_user.nil? ? User.where('email = ?', auth["info"]["email"]).first : current_user
       if user.blank?
        user = User.new
        user.password = Devise.friendly_token[0,10]
-       user.name = auth.info.name
-       user.email = auth.info.email
+       # user.name = auth.info.name
+       user.email = auth["info"]["email"]
        user.skip_confirmation! # skip confirmation through email during signup via facebook.
-       auth.provider == "twitter" ?  user.save(:validate => false) :  user.save
+       user.save(:validate => false)
      end
-     authorization.username = auth.info.nickname
+     # authorization.username = auth.info.nickname
      authorization.user_id = user.id
      authorization.save
     end
