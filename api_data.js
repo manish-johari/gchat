1bfa52c331a224a2fcf1b5b1f80438a77a1d0dd0
Manish Johari <manish.johari@kiwitech.com>
2014-12-04 21:15:26 +0530

    create profile api

--- api_data.js
+++ api_data.js
@@ -252,6 +252,13 @@ define({ api: [
           {
             "group": "Parameter",
             "type": "String",
+            "field": "profile[language_id]",
+            "optional": false,
+            "description": "<p>provide id of language selected by user</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
             "field": "profile[first_name]",
             "optional": false,
             "description": "<p>provide first name of the user</p>"
@@ -301,13 +308,6 @@ define({ api: [
           {
             "group": "Parameter",
             "type": "String",
-            "field": "profile[language_id]",
-            "optional": false,
-            "description": "<p>provide id of language selected by user</p>"
-          },
-          {
-            "group": "Parameter",
-            "type": "String",
             "field": "profile[relationship_id]",
             "optional": true,
             "description": "<p>provide id for relationship status of the user</p>"
@@ -318,6 +318,69 @@ define({ api: [
             "field": "profile[education_id]",
             "optional": true,
             "description": "<p>provide education_id of the user</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "field": "profile[orientation]",
+            "optional": true,
+            "description": "<p>provide value of orientation eg. xyzabc</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "field": "profile[career_id]",
+            "optional": true,
+            "description": "<p>provide career_id of the user</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "field": "profile[boy]",
+            "optional": true,
+            "description": "<p>provide number eg. 2</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "field": "profile[girl]",
+            "optional": true,
+            "description": "<p>number eg. 2</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Boolean",
+            "field": "profile[indentical_twins]",
+            "optional": true,
+            "description": "<p>true/false</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Boolean",
+            "field": "profile[fraternal_twins]",
+            "optional": true,
+            "description": "<p>true/false</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Boolean",
+            "field": "profile[triplets]",
+            "optional": true,
+            "description": "<p>true/false</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Boolean",
+            "field": "profile[quads]",
+            "optional": true,
+            "description": "<p>true/false</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "field": "profile[religion_id]",
+            "optional": true,
+            "description": "<p>provide id of religion eg. 1</p>"
           },
           {
             "group": "Parameter",
@@ -642,6 +705,24 @@ define({ api: [
         {
           "title": "Success-Response (200):",
           "content": "{\n\t \"pets\": [{\"id\":1,\"name\":\"Bird\"},{\"id\":2,\"name\":\"Cat\"},{\"id\":3,\"name\":\"Chicken\"},{\"id\":4,\"name\":\"Chinchilla\"},{\"id\":5,\"name\":\"Cow\"},{\"id\":6,\"name\":\"Dog\"},{\"id\":8,\"name\":\"Ferret\"},{\"id\":7,\"name\":\"Fish\"},{\"id\":9,\"name\":\"Frog\"},{\"id\":10,\"name\":\"Gerbil\"},{\"id\":11,\"name\":\"Guinea Pig\"},{\"id\":12,\"name\":\"Hamster\"},{\"id\":13,\"name\":\"Hermit Crab\"},{\"id\":14,\"name\":\"Horse\"},{\"id\":15,\"name\":\"Monkey\"},{\"id\":16,\"name\":\"Rabbit\"},{\"id\":17,\"name\":\"Rat\"},{\"id\":18,\"name\":\"Rooster\"},{\"id\":19,\"name\":\"Snake\"},{\"id\":20,\"name\":\"Spider\"},{\"id\":21,\"name\":\"Turtle\"}]\n}\n"
+        }
+      ]
+    },
+  },
+
+  {
+    "type": "get",
+    "url": "/profile_data",
+    "title": "Get Profile Data",
+    "name": "profile_data",
+    "description": "Retrieve All static data.",
+    "group": "Profile",
+    "version": "1",
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response (200):",
+          "content": "{\n\t \"languages\":[{\"id\":1,\"name\":\"Arabic\",\"code\":\"ar\"},{\"id\":2,\"name\":\"Bulgarian\",\"code\":\"bg\"},{\"id\":3,\"name\":\"Catalan\",\"code\":\"ca\"},{\"id\":4,\"name\":\"Chinese (Simplified)\",\"code\":\"zh-CHS\"},{\"id\":5,\"name\":\"Chinese (Traditional)\",\"code\":\"zh-CHT\"},{\"id\":6,\"name\":\"Czech\",\"code\":\"cs\"},{\"id\":7,\"name\":\"Danish\",\"code\":\"da\"},{\"id\":8,\"name\":\"Dutch\",\"code\":\"nl\"},{\"id\":9,\"name\":\"English\",\"code\":\"en\"},{\"id\":10,\"name\":\"Estonian\",\"code\":\"et\"},{\"id\":12,\"name\":\"Finnish\",\"code\":\"fi\"},{\"id\":13,\"name\":\"French\",\"code\":\"fr\"},{\"id\":14,\"name\":\"German\",\"code\":\"de\"},{\"id\":15,\"name\":\"Greek\",\"code\":\"el\"},{\"id\":16,\"name\":\"Haitian Creole\",\"code\":\"ht\"},{\"id\":17,\"name\":\"Hebrew\",\"code\":\"he\"},{\"id\":18,\"name\":\"Hindi\",\"code\":\"hi\"},{\"id\":27,\"name\":\"Hmong Daw\",\"code\":\"mww\"},{\"id\":19,\"name\":\"Hungarian\",\"code\":\"hu\"},{\"id\":20,\"name\":\"Indonesian\",\"code\":\"id\"},{\"id\":21,\"name\":\"Italian\",\"code\":\"it\"},{\"id\":22,\"name\":\"Japanese\",\"code\":\"ja\"},{\"id\":23,\"name\":\"Korean\",\"code\":\"ko\"},{\"id\":24,\"name\":\"Latvian\",\"code\":\"lv\"},{\"id\":25,\"name\":\"Lithuanian\",\"code\":\"lt\"},{\"id\":26,\"name\":\"Malay\",\"code\":\"ms\"},{\"id\":28,\"name\":\"Norwegian\",\"code\":\"no\"},{\"id\":11,\"name\":\"Persian (Farsi)\",\"code\":\"fa\"},{\"id\":29,\"name\":\"Polish\",\"code\":\"pl\"},{\"id\":30,\"name\":\"Portuguese\",\"code\":\"pt\"},{\"id\":31,\"name\":\"Romanian\",\"code\":\"ro\"},{\"id\":32,\"name\":\"Russian\",\"code\":\"ru\"},{\"id\":33,\"name\":\"Slovak\",\"code\":\"sk\"},{\"id\":34,\"name\":\"Slovenian\",\"code\":\"sl\"},{\"id\":35,\"name\":\"Spanish\",\"code\":\"es\"},{\"id\":36,\"name\":\"Swedish\",\"code\":\"sv\"},{\"id\":37,\"name\":\"Thai\",\"code\":\"th\"},{\"id\":38,\"name\":\"Turkish\",\"code\":\"tr\"},{\"id\":39,\"name\":\"Ukrainian\",\"code\":\"uk\"},{\"id\":40,\"name\":\"Urdu\",\"code\":\"ur\"},{\"id\":41,\"name\":\"Vietnamese\",\"code\":\"vi\"}],\"educations\":[{\"id\":1,\"name\":\"Elementary\"},{\"id\":2,\"name\":\"Middle or Junior High\"},{\"id\":3,\"name\":\"High School\"},{\"id\":4,\"name\":\"College/University\"}],\"relationships\":[{\"id\":1,\"name\":\"Single\"},{\"id\":2,\"name\":\"Married\"},{\"id\":3,\"name\":\"In Relationship\"},{\"id\":4,\"name\":\"Divorced\"},{\"id\":5,\"name\":\"Separated\"},{\"id\":6,\"name\":\"Engaged\"},{\"id\":7,\"name\":\"It's Complicated\"},{\"id\":8,\"name\":\"Open Relationship\"},{\"id\":9,\"name\":\"Widowed\"}],\"careers\":[{\"id\":1,\"name\":\"Career_1\"},{\"id\":10,\"name\":\"Career_10\"},{\"id\":2,\"name\":\"Career_2\"},{\"id\":3,\"name\":\"Career_3\"},{\"id\":4,\"name\":\"Career_4\"},{\"id\":5,\"name\":\"Career_5\"},{\"id\":6,\"name\":\"Career_6\"},{\"id\":7,\"name\":\"Career_7\"},{\"id\":8,\"name\":\"Career_8\"},{\"id\":9,\"name\":\"Career_9\"}],\"collectings\":[{\"id\":1,\"name\":\"Collecting_1\"},{\"id\":10,\"name\":\"Collecting_10\"},{\"id\":2,\"name\":\"Collecting_2\"},{\"id\":3,\"name\":\"Collecting_3\"},{\"id\":4,\"name\":\"Collecting_4\"},{\"id\":5,\"name\":\"Collecting_5\"},{\"id\":6,\"name\":\"Collecting_6\"},{\"id\":7,\"name\":\"Collecting_7\"},{\"id\":8,\"name\":\"Collecting_8\"},{\"id\":9,\"name\":\"Collecting_9\"}],\"hobbies\":[{\"id\":1,\"name\":\"hobbies_1\"},{\"id\":10,\"name\":\"hobbies_10\"},{\"id\":2,\"name\":\"hobbies_2\"},{\"id\":3,\"name\":\"hobbies_3\"},{\"id\":4,\"name\":\"hobbies_4\"},{\"id\":5,\"name\":\"hobbies_5\"},{\"id\":6,\"name\":\"hobbies_6\"},{\"id\":7,\"name\":\"hobbies_7\"},{\"id\":8,\"name\":\"hobbies_8\"},{\"id\":9,\"name\":\"hobbies_9\"}],\"movies\":[{\"id\":1,\"name\":\"Movie_1\"},{\"id\":10,\"name\":\"Movie_10\"},{\"id\":2,\"name\":\"Movie_2\"},{\"id\":3,\"name\":\"Movie_3\"},{\"id\":4,\"name\":\"Movie_4\"},{\"id\":5,\"name\":\"Movie_5\"},{\"id\":6,\"name\":\"Movie_6\"},{\"id\":7,\"name\":\"Movie_7\"},{\"id\":8,\"name\":\"Movie_8\"},{\"id\":9,\"name\":\"Movie_9\"}],\"music\":[{\"id\":1,\"name\":\"Music_1\"},{\"id\":10,\"name\":\"Music_10\"},{\"id\":2,\"name\":\"Music_2\"},{\"id\":3,\"name\":\"Music_3\"},{\"id\":4,\"name\":\"Music_4\"},{\"id\":5,\"name\":\"Music_5\"},{\"id\":6,\"name\":\"Music_6\"},{\"id\":7,\"name\":\"Music_7\"},{\"id\":8,\"name\":\"Music_8\"},{\"id\":9,\"name\":\"Music_9\"}],\"pets\":[{\"id\":1,\"name\":\"Bird\"},{\"id\":2,\"name\":\"Cat\"},{\"id\":3,\"name\":\"Chicken\"},{\"id\":4,\"name\":\"Chinchilla\"},{\"id\":5,\"name\":\"Cow\"},{\"id\":6,\"name\":\"Dog\"},{\"id\":8,\"name\":\"Ferret\"},{\"id\":7,\"name\":\"Fish\"},{\"id\":9,\"name\":\"Frog\"},{\"id\":10,\"name\":\"Gerbil\"},{\"id\":11,\"name\":\"Guinea Pig\"},{\"id\":12,\"name\":\"Hamster\"},{\"id\":13,\"name\":\"Hermit Crab\"},{\"id\":14,\"name\":\"Horse\"},{\"id\":15,\"name\":\"Monkey\"},{\"id\":16,\"name\":\"Rabbit\"},{\"id\":17,\"name\":\"Rat\"},{\"id\":18,\"name\":\"Rooster\"},{\"id\":19,\"name\":\"Snake\"},{\"id\":20,\"name\":\"Spider\"},{\"id\":21,\"name\":\"Turtle\"}],\"political_views\":[{\"id\":1,\"name\":\"PoliticalView_1\"},{\"id\":10,\"name\":\"PoliticalView_10\"},{\"id\":2,\"name\":\"PoliticalView_2\"},{\"id\":3,\"name\":\"PoliticalView_3\"},{\"id\":4,\"name\":\"PoliticalView_4\"},{\"id\":5,\"name\":\"PoliticalView_5\"},{\"id\":6,\"name\":\"PoliticalView_6\"},{\"id\":7,\"name\":\"PoliticalView_7\"},{\"id\":8,\"name\":\"PoliticalView_8\"},{\"id\":9,\"name\":\"PoliticalView_9\"}],\"sports\":[{\"id\":1,\"name\":\"Sport_1\"},{\"id\":10,\"name\":\"Sport_10\"},{\"id\":2,\"name\":\"Sport_2\"},{\"id\":3,\"name\":\"Sport_3\"},{\"id\":4,\"name\":\"Sport_4\"},{\"id\":5,\"name\":\"Sport_5\"},{\"id\":6,\"name\":\"Sport_6\"},{\"id\":7,\"name\":\"Sport_7\"},{\"id\":8,\"name\":\"Sport_8\"},{\"id\":9,\"name\":\"Sport_9\"}]\n}\n"
         }
       ]
     },
